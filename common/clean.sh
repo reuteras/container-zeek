f508d030f237521d9478d5eebb16ed2601b40fc3
Peter Reuterås <peter@reuteras.com>
2022-02-16 15:11:07 +0100

    Fix clean

--- common/clean.sh
+++ common/clean.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
-find /usr/local/zeek-"${ZEEK_VER}"/{bin,lib} -type f -exec file {} \; | grep stripped | cut -f1 -d: | xargs strip -s
-find /usr/local/zeek-"${ZEEK_VER}" -type d -name .git -exec rm -rf {} \;
+find /usr/local/zeek-"${ZEEK_VER}"/{bin,lib} /usr/local/spicy-"${SPICY_VER}"/lib/* -type f -exec file {} \; | grep stripped | cut -f1 -d: | xargs strip -s
+find /usr/local/zeek-"${ZEEK_VER}" -type d -name .git -print0 | xargs rm -rf 
 rm -rf /usr/local/zeek-"${ZEEK_VER}"/var/lib/zkg/clones/package/*/build
-strip -s "${SPICY_DIR}"/lib/*
