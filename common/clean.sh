f588f0895abbe4a69ae33e83a3113545b611789f
Peter Reuterås <peter@reuteras.com>
2023-09-12 12:38:17 +0200

    Remove old code

--- common/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-find /usr/local/zeek-"${ZEEK_VER}"/{bin,lib} /usr/local/spicy-"${SPICY_VER}"/lib/* -type f -exec file {} \; | grep stripped | cut -f1 -d: | xargs strip -s
-find /usr/local/zeek-"${ZEEK_VER}" -type d -name .git -print0 | xargs rm -rf 
-rm -rf /usr/local/zeek-"${ZEEK_VER}"/var/lib/zkg/clones/package/*/build
-rm -f /usr/local/spicy-main/lib/lib*-debug.a
-rm -rf /usr/local/zeek-"${ZEEK_VER}"/lib/zeek/plugins/packages/spicy-plugin/dist/
